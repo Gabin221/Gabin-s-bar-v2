bbcebeb5fa6c083461552c11aff735809087ef88
Gabin221 <serrurot19@gmail.com>
2024-08-23 17:58:59 +0200

    Update

--- app/src/main/java/com/example/gabinsbarv2/MainActivity.kt
+++ app/src/main/java/com/example/gabinsbarv2/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.gabinsbarv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.gabinsbarv2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +17,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        navController= Navigation.findNavController(this,R.id.activity_main_nav_host_fragment)
-        setupWithNavController(binding.bottomNavigationView,navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.activity_main_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.softsFragment,
+                R.id.alcoolFragment,
+                R.id.panierFragment,
+                R.id.compteFragment
+            )
+        )
+
+        setupActionBarWithNavController(this, navController, appBarConfiguration)
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/gabinsbarv2/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/gabinsbarv2/fragments/HomeFragment.kt
@@ -5,16 +5,89 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.navigation.fragment.findNavController
 import com.example.gabinsbarv2.R
+import com.example.gabinsbarv2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var buttonHome2Sirops: LinearLayout
+    lateinit var buttonHome2Softs: LinearLayout
+    lateinit var buttonHome2Bieres: LinearLayout
+    lateinit var buttonHome2Vins: LinearLayout
+    lateinit var buttonHome2Classiques: LinearLayout
+    lateinit var buttonHome2Extravagants: LinearLayout
+    lateinit var buttonHome2Cafes: LinearLayout
+    lateinit var buttonHome2Thes: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        buttonHome2Sirops = root.findViewById(R.id.buttonHome2Sirops)
+        buttonHome2Softs = root.findViewById(R.id.buttonHome2Softs)
+        buttonHome2Bieres = root.findViewById(R.id.buttonHome2Bieres)
+        buttonHome2Vins = root.findViewById(R.id.buttonHome2Vins)
+        buttonHome2Classiques = root.findViewById(R.id.buttonHome2Classiques)
+        buttonHome2Extravagants = root.findViewById(R.id.buttonHome2Extravagants)
+        buttonHome2Cafes = root.findViewById(R.id.buttonHome2Cafes)
+        buttonHome2Thes = root.findViewById(R.id.buttonHome2Thes)
+
+        return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonHome2Sirops.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.softsFragment)
+        }
+
+        buttonHome2Softs.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.softsFragment)
+        }
+
+        buttonHome2Cafes.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.softsFragment)
+        }
+
+        buttonHome2Thes.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.softsFragment)
+        }
+
+        buttonHome2Bieres.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.alcoolFragment)
+        }
+
+        buttonHome2Vins.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.alcoolFragment)
+        }
+
+        buttonHome2Classiques.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.alcoolFragment)
+        }
+
+        buttonHome2Extravagants.setOnClickListener {
+            //findNavController().popBackStack(R.id.homeFragment, false)
+            findNavController().navigate(R.id.alcoolFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
