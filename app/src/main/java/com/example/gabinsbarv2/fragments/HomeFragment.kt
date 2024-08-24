d2764550b28744541c14ca01ab311bc4f8bb2691
Gabin221 <serrurot19@gmail.com>
2024-08-24 18:10:44 +0200

    Update

--- app/src/main/java/com/example/gabinsbarv2/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/gabinsbarv2/fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.gabinsbarv2.R
 import com.example.gabinsbarv2.databinding.FragmentHomeBinding
@@ -22,6 +23,15 @@ class HomeFragment : Fragment() {
     lateinit var buttonHome2Extravagants: LinearLayout
     lateinit var buttonHome2Cafes: LinearLayout
     lateinit var buttonHome2Thes: LinearLayout
+    lateinit var quantiteBoissons: TextView
+    lateinit var quantiteSirops: TextView
+    lateinit var quantiteSofts: TextView
+    lateinit var quantiteBieres: TextView
+    lateinit var quantiteVins: TextView
+    lateinit var quantiteClassiques: TextView
+    lateinit var quantiteExtravagants: TextView
+    lateinit var quantiteCafes: TextView
+    lateinit var quantiteThes: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,8 +48,36 @@ class HomeFragment : Fragment() {
         buttonHome2Extravagants = root.findViewById(R.id.buttonHome2Extravagants)
         buttonHome2Cafes = root.findViewById(R.id.buttonHome2Cafes)
         buttonHome2Thes = root.findViewById(R.id.buttonHome2Thes)
+        quantiteBoissons = root.findViewById(R.id.quantiteBoissons)
+        quantiteSirops = root.findViewById(R.id.quantiteSirops)
+        quantiteSofts = root.findViewById(R.id.quantiteSofts)
+        quantiteBieres = root.findViewById(R.id.quantiteBieres)
+        quantiteVins = root.findViewById(R.id.quantiteVins)
+        quantiteClassiques = root.findViewById(R.id.quantiteClassiques)
+        quantiteExtravagants = root.findViewById(R.id.quantiteExtravagants)
+        quantiteCafes = root.findViewById(R.id.quantiteCafes)
+        quantiteThes = root.findViewById(R.id.quantiteThes)
+
+        displayQuantites()
 
         return root
+    }
+
+    fun displayQuantites() {
+        val quantites = listOf(28, 3, 5, 7, 6, 4, 6, 4)
+        var quantiteTotal = 0
+        for (quantite in quantites) {
+            quantiteTotal += quantite
+        }
+        quantiteSirops.text = quantites[0].toString()
+        quantiteSofts.text = quantites[1].toString()
+        quantiteBieres.text = quantites[2].toString()
+        quantiteVins.text = quantites[3].toString()
+        quantiteClassiques.text = quantites[4].toString()
+        quantiteExtravagants.text = quantites[5].toString()
+        quantiteCafes.text = quantites[6].toString()
+        quantiteThes.text = quantites[7].toString()
+        quantiteBoissons.text = "$quantiteTotal boissons dispos."
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
