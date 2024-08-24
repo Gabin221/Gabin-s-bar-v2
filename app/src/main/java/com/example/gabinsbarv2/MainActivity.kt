2b9766f18c972ba3e29c8474670e21af2d2e93c7
Gabin221 <serrurot19@gmail.com>
2024-08-24 23:11:15 +0200

    Update

--- app/src/main/java/com/example/gabinsbarv2/MainActivity.kt
+++ app/src/main/java/com/example/gabinsbarv2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.gabinsbarv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,6 +11,9 @@ import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.gabinsbarv2.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +37,9 @@ class MainActivity : AppCompatActivity() {
                 R.id.compteFragment
             )
         )
+
+        val navBar  = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navBar.getOrCreateBadge(R.id.panierFragment).number = 2
 
         setupActionBarWithNavController(this, navController, appBarConfiguration)
         binding.bottomNavigationView.setupWithNavController(navController)
--- app/src/main/java/com/example/gabinsbarv2/fragments/PanierFragment.kt
+++ app/src/main/java/com/example/gabinsbarv2/fragments/PanierFragment.kt
@@ -6,17 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.gabinsbarv2.R
+import com.example.gabinsbarv2.databinding.FragmentAlcoolBinding
 
 
 class PanierFragment : Fragment() {
-
+    private var _binding: FragmentAlcoolBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+        _binding = FragmentAlcoolBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_panier, container, false)
+        return root
     }
 
 }
