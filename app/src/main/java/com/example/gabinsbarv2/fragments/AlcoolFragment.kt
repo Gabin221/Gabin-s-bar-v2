2d2b467f1c0f8edb4f23a38d4e2b0e7a80356a77
Gabin221 <serrurot19@gmail.com>
2024-08-24 22:30:08 +0200

    Update

--- app/src/main/java/com/example/gabinsbarv2/fragments/AlcoolFragment.kt
+++ app/src/main/java/com/example/gabinsbarv2/fragments/AlcoolFragment.kt
@@ -5,18 +5,93 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.gabinsbarv2.R
-
+import com.example.gabinsbarv2.databinding.FragmentAlcoolBinding
 
 class AlcoolFragment : Fragment() {
+    private var _binding: FragmentAlcoolBinding? = null
+    private val binding get() = _binding!!
 
+    lateinit var quantiteBoissons: TextView
+    lateinit var contenuBieres: LinearLayout
+    lateinit var contenuVins: LinearLayout
+    lateinit var contenuClassiques: LinearLayout
+    lateinit var contenuExtravagants: LinearLayout
+    lateinit var buttonBieres: LinearLayout
+    lateinit var buttonVins: LinearLayout
+    lateinit var buttonClassiques: LinearLayout
+    lateinit var buttonExtravagants: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alcool, container, false)
+        _binding = FragmentAlcoolBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        quantiteBoissons = root.findViewById(R.id.quantiteBoissons)
+        contenuBieres = root.findViewById(R.id.contenuBieres)
+        contenuVins = root.findViewById(R.id.contenuVins)
+        contenuClassiques = root.findViewById(R.id.contenuClassiques)
+        contenuExtravagants = root.findViewById(R.id.contenuExtravagants)
+        buttonBieres = root.findViewById(R.id.buttonBieres)
+        buttonVins = root.findViewById(R.id.buttonVins)
+        buttonClassiques = root.findViewById(R.id.buttonClassiques)
+        buttonExtravagants = root.findViewById(R.id.buttonExtravagants)
+
+        displayQuantites()
+        gestionClicks()
+
+        return root
+    }
+
+    fun gestionClicks() {
+        buttonBieres.setOnClickListener {
+            if (contenuBieres.visibility == View.GONE) {
+                contenuBieres.visibility = View.VISIBLE
+                contenuVins.visibility = View.GONE
+                contenuClassiques.visibility = View.GONE
+                contenuExtravagants.visibility = View.GONE
+            }
+        }
+
+        buttonVins.setOnClickListener {
+            if (contenuVins.visibility == View.GONE) {
+                contenuVins.visibility = View.VISIBLE
+                contenuBieres.visibility = View.GONE
+                contenuClassiques.visibility = View.GONE
+                contenuExtravagants.visibility = View.GONE
+            }
+        }
+
+        buttonClassiques.setOnClickListener {
+            if (contenuClassiques.visibility == View.GONE) {
+                contenuClassiques.visibility = View.VISIBLE
+                contenuVins.visibility = View.GONE
+                contenuBieres.visibility = View.GONE
+                contenuExtravagants.visibility = View.GONE
+            }
+        }
+
+        buttonExtravagants.setOnClickListener {
+            if (contenuExtravagants.visibility == View.GONE) {
+                contenuExtravagants.visibility = View.VISIBLE
+                contenuVins.visibility = View.GONE
+                contenuBieres.visibility = View.GONE
+                contenuClassiques.visibility = View.GONE
+            }
+        }
+    }
+
+    fun displayQuantites() {
+        val quantites = listOf(5, 7, 6, 4)
+        var quantiteTotal = 0
+        for (quantite in quantites) {
+            quantiteTotal += quantite
+        }
+        quantiteBoissons.text = "$quantiteTotal boissons dispos."
     }
 
 }
--- app/src/main/java/com/example/gabinsbarv2/fragments/SoftsFragment.kt
+++ app/src/main/java/com/example/gabinsbarv2/fragments/SoftsFragment.kt
@@ -5,17 +5,93 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.gabinsbarv2.R
+import com.example.gabinsbarv2.databinding.FragmentSoftsBinding
 
 class SoftsFragment : Fragment() {
+    private var _binding: FragmentSoftsBinding? = null
+    private val binding get() = _binding!!
 
+    lateinit var quantiteBoissons: TextView
+    lateinit var contenuSirops: LinearLayout
+    lateinit var contenuSoft: LinearLayout
+    lateinit var contenuCafes: LinearLayout
+    lateinit var contenuThes: LinearLayout
+    lateinit var buttonSirops: LinearLayout
+    lateinit var buttonSoft: LinearLayout
+    lateinit var buttonCafes: LinearLayout
+    lateinit var buttonThes: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_softs, container, false)
+        _binding = FragmentSoftsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        quantiteBoissons = root.findViewById(R.id.quantiteBoissons)
+        contenuSirops = root.findViewById(R.id.contenuSirops)
+        contenuSoft = root.findViewById(R.id.contenuSofts)
+        contenuCafes = root.findViewById(R.id.contenuCafes)
+        contenuThes = root.findViewById(R.id.contenuThes)
+        buttonSirops = root.findViewById(R.id.buttonSirops)
+        buttonSoft = root.findViewById(R.id.buttonSofts)
+        buttonCafes = root.findViewById(R.id.buttonCafes)
+        buttonThes = root.findViewById(R.id.buttonThes)
+
+        displayQuantites()
+        gestionClicks()
+
+        return root
+    }
+
+    fun gestionClicks() {
+        buttonSirops.setOnClickListener {
+            if (contenuSirops.visibility == View.GONE) {
+                contenuSirops.visibility = View.VISIBLE
+                contenuSoft.visibility = View.GONE
+                contenuCafes.visibility = View.GONE
+                contenuThes.visibility = View.GONE
+            }
+        }
+
+        buttonSoft.setOnClickListener {
+            if (contenuSoft.visibility == View.GONE) {
+                contenuSoft.visibility = View.VISIBLE
+                contenuSirops.visibility = View.GONE
+                contenuCafes.visibility = View.GONE
+                contenuThes.visibility = View.GONE
+            }
+        }
+
+        buttonCafes.setOnClickListener {
+            if (contenuCafes.visibility == View.GONE) {
+                contenuCafes.visibility = View.VISIBLE
+                contenuSoft.visibility = View.GONE
+                contenuSirops.visibility = View.GONE
+                contenuThes.visibility = View.GONE
+            }
+        }
+
+        buttonThes.setOnClickListener {
+            if (contenuThes.visibility == View.GONE) {
+                contenuThes.visibility = View.VISIBLE
+                contenuSoft.visibility = View.GONE
+                contenuSirops.visibility = View.GONE
+                contenuCafes.visibility = View.GONE
+            }
+        }
+    }
+
+    fun displayQuantites() {
+        val quantites = listOf(28, 3, 6, 4)
+        var quantiteTotal = 0
+        for (quantite in quantites) {
+            quantiteTotal += quantite
+        }
+        quantiteBoissons.text = "$quantiteTotal boissons dispos."
     }
 
 }
